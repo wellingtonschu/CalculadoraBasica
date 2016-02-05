2afe734abbc636a8e04cb45888c24790c0880d07
Wellington <wellington.schu@live.com>
2016-02-05 13:40:47 -0200

    Implementação Gráfica Inicial

---
+++ CalculadoraBasica/Calculadora/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraBasica
+{
+    public class Calculadora
+    {
+        private readonly decimal _primeiroNumero;
+        private readonly decimal _segundoNumero;
+
+        public Calculadora(Decimal primeiroNumero, Decimal segundoNumero)
+        {
+            _primeiroNumero = primeiroNumero;
+            _segundoNumero = segundoNumero;
+        }
+
+        public Decimal Calcular()
+        {
+            return _primeiroNumero + _segundoNumero;
+        }
+
+        public Decimal Multiplicar()
+        {
+            return _primeiroNumero * _segundoNumero;
+        }
+
+        public Decimal Subtrair()
+        {
+            return _primeiroNumero - _segundoNumero;
+        }
+
+        public Decimal Dividir()
+        {
+            return _primeiroNumero / _segundoNumero;
+        }
+    }
+}
---
+++ CalculadoraBasica/CalculadoraGrafica/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CalculadoraGrafica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPrimeiroNumero = new System.Windows.Forms.TextBox();
+            this.txtSegundoNumero = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPrimeiroNumero
+            // 
+            this.txtPrimeiroNumero.Location = new System.Drawing.Point(12, 29);
+            this.txtPrimeiroNumero.Multiline = true;
+            this.txtPrimeiroNumero.Name = "txtPrimeiroNumero";
+            this.txtPrimeiroNumero.Size = new System.Drawing.Size(260, 48);
+            this.txtPrimeiroNumero.TabIndex = 0;
+            // 
+            // txtSegundoNumero
+            // 
+            this.txtSegundoNumero.Location = new System.Drawing.Point(12, 100);
+            this.txtSegundoNumero.Multiline = true;
+            this.txtSegundoNumero.Name = "txtSegundoNumero";
+            this.txtSegundoNumero.Size = new System.Drawing.Size(260, 48);
+            this.txtSegundoNumero.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(12, 175);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(136, 56);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Informe o primeiro número";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Informe o segundo número";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.Blue;
+            this.lblResultado.Location = new System.Drawing.Point(173, 182);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 31);
+            this.lblResultado.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 243);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSegundoNumero);
+            this.Controls.Add(this.txtPrimeiroNumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPrimeiroNumero;
+        private System.Windows.Forms.TextBox txtSegundoNumero;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
